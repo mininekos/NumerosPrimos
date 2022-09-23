8b3305320fbf06d5197b9093344c872bcb6a317a
mininekos <manuel2g2m@gmail.com>
2022-09-23 09:29:19 +0200

    Agregar archivos de proyecto.

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] primos=new int[5];
+int numPrimo = 0,cont;
+for (int i = 1; numPrimo < 5; i++) {
+    cont = 0;
+	for (int j = 1; j < i; j++)
+	{
+		if (i % j == 0) {
+			cont++;
+
+        }
+		
+	}
+	if (cont == 2)
+	{
+		primos[numPrimo] = i;
+	}
+}
+Console.WriteLine(primos.ToString);
